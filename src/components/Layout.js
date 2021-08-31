34ce36be7fdb013f70b0063015502c69533031fa
Stackbit Code Editor <projects@stackbit.com>
2021-08-31 09:20:15 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -42,7 +42,7 @@ export default class Body extends React.Component {
                     )}
                     {(font === 'fraunces') ? (
                     <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet"/>
-                    ) : ((font === 'karla') && (
+                    ) : ((font === 'open-sans') && (
                     <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
                     ))}
                     {_.get(this.props, 'pageContext.site.siteMetadata.favicon', null) && (
