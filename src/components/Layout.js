45a49711bdfc78697133e0056bca3345a8b3d8a4
Stackbit Code Editor <projects@stackbit.com>
2021-08-31 09:21:21 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -43,7 +43,7 @@ export default class Body extends React.Component {
                     {(font === 'fraunces') ? (
                     <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet"/>
                     ) : ((font === 'open-sans') && (
-                    <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
                     ))}
                     {_.get(this.props, 'pageContext.site.siteMetadata.favicon', null) && (
                     <link rel="icon" href={withPrefix(_.get(this.props, 'pageContext.site.siteMetadata.favicon', null))}/>
